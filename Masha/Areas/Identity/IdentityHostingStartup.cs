8b68327cb759e7fe821e66aa47cbf8c366b9f62f
Anton Novikov <novikov.anton.sat@gmail.com>
2020-05-24 12:45:39 +0300

    0.0.1 Identity init

---
+++ Masha/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Masha.Data;
+using Masha.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Masha.Areas.Identity.IdentityHostingStartup))]
+namespace Masha.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
